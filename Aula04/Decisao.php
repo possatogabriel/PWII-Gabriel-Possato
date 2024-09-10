6e1bd7cacc9c2aa79cb1ac94c95c2cac6f7dcb44
possatogabriel <chaymitoyt@gmail.com>
2024-09-10 14:28:17 -0300

    Teste o PHP - Parte II

---
+++ Aula04/Decisao.php
@@ -0,0 +1,19 @@
+<?php
+    $n = 6;
+
+    if ($n >= 4) {
+        echo "O número é maior ou igual a 4, sendo ele: ", $n;
+    }
+
+    else {
+        echo "O número é 5 ou 6, sendo ele: ", $n;
+    }
+
+    $n1 = 4;
+    $n2 = 4;
+
+    $result = ($n1 + $n2);
+
+    echo result;
+
+// DICA DE MERCADO: NUNCA USAR ELSE
---
+++ Aula04/Variaveis.php
@@ -0,0 +1,7 @@
+<?php
+    $variavel_1 = "Texto";
+
+    $variavel_2 = 2;
+
+    echo $variavel_1;
+    echo "<br>";
